a4a56f5ac9d145e36176dad7d8f19004fcae9c0b
Waffle <60797216+wafflethings@users.noreply.github.com>
2025-12-02 23:02:37 +0000

    premake day3 for laptop

---
+++ Aoc25.Day3/Program.cs
@@ -0,0 +1,11 @@
+﻿using Aoc25.Common;
+
+namespace Aoc25.Day3;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!".StringRepeatsTwice());
+    }
+}
